11afa66f6a5af7011df0e9c109b37eeb4261fba8
Aliaksandr Zelianko <zely20@mail.ru>
2021-09-07 13:52:22 +0300

    Обработка исключений и Spring REST

--- src/main/java/ru/job4j/job4j_chat/controller/PersonController.java
+++ src/main/java/ru/job4j/job4j_chat/controller/PersonController.java
@@ -1,13 +1,20 @@
 package ru.job4j.job4j_chat.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import ru.job4j.job4j_chat.entity.Person;
 import ru.job4j.job4j_chat.repository.PersonRepository;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -15,9 +22,12 @@ import java.util.stream.StreamSupport;
 @RequestMapping("/person")
 public class PersonController {
 
+    private final ObjectMapper objectMapper;
     private final PersonRepository personRepository;
+    private static final Logger LOGGER = LoggerFactory.getLogger(PersonController.class.getSimpleName());
 
-    public PersonController(PersonRepository personRepository) {
+    public PersonController(ObjectMapper objectMapper, PersonRepository personRepository) {
+        this.objectMapper = objectMapper;
         this.personRepository = personRepository;
     }
 
@@ -29,20 +39,32 @@ public class PersonController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Person> findById(@PathVariable int id) {
-        Optional<Person> message = personRepository.findById(id);
-        return new ResponseEntity<>(message.orElse(new Person()),
-                message.isEmpty() ? HttpStatus.OK : HttpStatus.NOT_FOUND);
+    public Person findById(@PathVariable int id) {
+        return personRepository.findById(id).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "Person is not found."));
     }
 
     @PutMapping("/")
     public ResponseEntity<Void> update (@RequestBody Person person) {
+        var username = person.getNickname();
+        var password = person.getPassword();
+        if (username == null || password == null) {
+            throw new NullPointerException("Username and password mustn't be empty");
+        }
         personRepository.save(person);
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/")
     public ResponseEntity<Person> create(@RequestBody Person person) {
+        var username = person.getNickname();
+        var password = person.getPassword();
+        if (username == null || password == null) {
+            throw new NullPointerException("Username and password mustn't be empty");
+        }
+        if(username.length() < 4) {
+            throw new IllegalArgumentException("Invalid nickname, Nickname must be more 5 characters.");
+        }
         return new ResponseEntity<>(personRepository.save(person),
                 HttpStatus.CREATED);
     }
@@ -53,5 +75,16 @@ public class PersonController {
         person.setId(id);
         personRepository.delete(person);
         return ResponseEntity.ok().build();
+    }
+
+    @ExceptionHandler(value = { IllegalArgumentException.class })
+    public void exceptionHandler(Exception e, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setStatus(HttpStatus.BAD_REQUEST.value());
+        response.setContentType("application/json");
+        response.getWriter().write(objectMapper.writeValueAsString(new HashMap<>() { {
+            put("message", e.getMessage());
+            put("type", e.getClass());
+        }}));
+        LOGGER.error(e.getLocalizedMessage());
     }
 }
